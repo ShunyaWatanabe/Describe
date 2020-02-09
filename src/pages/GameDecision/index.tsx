62a1ee049a6a327fe60ddf3fe7ce5c7a7593a836
shunyawatanabe <s.watte3212@gmail.com>
2020-02-09 21:42:03 +0400

    feature: Add sounds to GameMain and GameDecision

--- src/pages/GameDecision/index.tsx
+++ src/pages/GameDecision/index.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import Button from 'src/components/Button';
 import styles from 'src/styles';
 import Props from 'src/interfaces/Props';
+import { Audio } from 'expo-av';
+
+const alarmClock = require('assets/sounds/alarmClock.wav');
+
+Audio.setAudioModeAsync({ playsInSilentModeIOS: true } as any);
+const soundObject = new Audio.Sound();
 
 export default function Screen(props: Props) {
   const {
     navigation: { navigate },
   } = props;
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await soundObject.loadAsync(alarmClock);
+        await soundObject.playAsync();
+      } catch (error) {
+        // An error occurred!
+        // console.log(error);
+      }
+    })();
+    return () => {
+      soundObject.stopAsync().then(() => soundObject.unloadAsync());
+    };
+  }, []);
 
   return (
     <View style={styles.container}>
--- src/pages/GameMain/index.tsx
+++ src/pages/GameMain/index.tsx
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { changeCurrentPhrase } from 'src/redux/actions';
 import { View, StatusBar } from 'react-native';
 import Card from 'src/components/Card';
 import styles from 'src/styles';
 import Props from 'src/interfaces/Props';
+import { Audio } from 'expo-av';
+
+const clock = require('assets/sounds/clock.wav');
+
+Audio.setAudioModeAsync({ playsInSilentModeIOS: true } as any);
+const soundObject = new Audio.Sound();
 
 function Screen(props: Props) {
   const {
-    // navigation: { navigate },
+    navigation: { navigate },
     currentPhrase,
   } = props;
+
+  useEffect(() => {
+    props.changeCurrentPhrase(currentPhrase);
+    let interval: any;
+    let rate: number = 1;
+    let times: number = 0;
+    (async () => {
+      try {
+        await soundObject.loadAsync(clock, {
+          isLooping: true,
+        });
+        await soundObject.playAsync();
+        interval = setInterval(async () => {
+          times += 1;
+          if (times < 3) {
+            rate += 0.5;
+            soundObject.setRateAsync(rate, true);
+          } else {
+            await soundObject.stopAsync();
+            await soundObject.unloadAsync();
+            clearInterval(interval);
+            navigate('GameDecision', {});
+          }
+        }, 5 * 1000);
+      } catch (error) {
+        // An error occurred!
+        // console.log(error);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar hidden />
